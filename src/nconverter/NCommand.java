851645a53f38707155fb1cfaa03e8e7a8276cfe0
unknown <loooool2312@gmail.com>
2024-03-01 04:41:49 +0900

    Extend NCommand.

--- src/nconverter/NCommand.java
+++ src/nconverter/NCommand.java
@@ -2,24 +2,32 @@ package nconverter;
 
 import javax.microedition.lcdui.*;
 
-public abstract class NCommand implements ItemCommandListener {
+public abstract class NCommand implements CommandListener, ItemCommandListener {
 	private final NConverter converter;
 	private final Command command;
 
-	public NCommand(NConverter converter, Command command) {
+	public NCommand(final NConverter converter, final Command command) {
 		this.converter = converter;
 		this.command = command;
 	}
 
-	public void commandAction(Command arg0, Item arg1) {
-		if (arg0 != command)
-			return;
+	public void tryAction() {
 		try {
-			tryAction(arg0, arg1);
+			action();
 		} catch (Exception e) {
 			converter.handleError(e);
 		}
 	}
 
-	public abstract void tryAction(Command arg0, Item arg1);
+	public void commandAction(Command arg0, Displayable arg1) {
+		if (arg0 == command)
+			tryAction();
+	}
+
+	public void commandAction(Command arg0, Item arg1) {
+		if (arg0 == command)
+			tryAction();
+	}
+
+	public abstract void action() throws Exception;
 }
--- src/nconverter/NForm.java
+++ src/nconverter/NForm.java
@@ -1,48 +1,35 @@
 package nconverter;
 
 import javax.microedition.lcdui.*;
-import javax.microedition.midlet.*;
 
-public class NForm extends Form implements CommandListener {
-	private final NConverter converter;
-
-	public NForm(String title, NConverter converter) {
+public class NForm extends Form {
+	public NForm(final String title, final NConverter converter) {
 		super(title);
-		this.converter = converter;
-		
-		final TextField binary = new TextField("Binary", "", 32, TextField.DECIMAL);
-		final TextField decimal = new TextField("Decimal", "", 16, TextField.DECIMAL);
-		final TextField hexadecimal = new TextField("Hex", "", 16, TextField.ANY);
-
-		initTextFields(binary, decimal, hexadecimal);
-
-		append(binary);
-		append(decimal);
-		append(hexadecimal);
-
-		addCommand(new Command("Exit", Command.EXIT, 2));
-		setCommandListener(this);
+		initCommands(converter);
+		initTextFields(converter);
 	}
 
-	public void commandAction(Command arg0, Displayable arg1) {
-		if (arg0.getCommandType() != Command.EXIT)
-			return;
-		try {
-			converter.destroyApp(false);
-		} catch (MIDletStateChangeException e) {
-			converter.handleError(e);
-		}
+	private void initCommands(final NConverter converter) {
+		final Command exit = new Command("Exit", Command.EXIT, 2);
+
+		setCommandListener(new NCommand(converter, exit) {
+			public void action() throws Exception {
+				converter.destroyApp(false);
+			}
+		});
+
+		addCommand(exit);
 	}
 
-	private void initTextFields(final TextField b, final TextField d, final TextField h) {
+	private void initTextFields(final NConverter converter) {
+		final TextField b = new TextField("Binary", "", 32, TextField.DECIMAL);
+		final TextField d = new TextField("Decimal", "", 16, TextField.DECIMAL);
+		final TextField h = new TextField("Hex", "", 16, TextField.ANY);
+
 		final Command convert = new Command("Convert", Command.ITEM, 1);
 
-		b.setDefaultCommand(convert);
-		d.setDefaultCommand(convert);
-		h.setDefaultCommand(convert);
-
 		b.setItemCommandListener(new NCommand(converter, convert) {
-			public void tryAction(Command c, Item item) {
+			public void action() {
 				final long l = Long.parseLong(b.getString(), 2);
 				d.setString(Long.toString(l, 10));
 				h.setString(Long.toString(l, 16));
@@ -50,7 +37,7 @@ public class NForm extends Form implements CommandListener {
 		});
 
 		d.setItemCommandListener(new NCommand(converter, convert) {
-			public void tryAction(Command c, Item item) {
+			public void action() {
 				final long l = Long.parseLong(d.getString(), 10);
 				b.setString(Long.toString(l, 2));
 				h.setString(Long.toString(l, 16));
@@ -58,11 +45,19 @@ public class NForm extends Form implements CommandListener {
 		});
 
 		h.setItemCommandListener(new NCommand(converter, convert) {
-			public void tryAction(Command c, Item item) {
+			public void action() {
 				final long l = Long.parseLong(h.getString(), 16);
 				b.setString(Long.toString(l, 2));
 				d.setString(Long.toString(l, 10));
 			}
 		});
+
+		b.setDefaultCommand(convert);
+		d.setDefaultCommand(convert);
+		h.setDefaultCommand(convert);
+
+		append(b);
+		append(d);
+		append(h);
 	}
 }
